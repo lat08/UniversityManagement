bb5df1519565b89b8efee7d01439f245861cce7e
ThienPDM <phandienmanhthienk16@siu.edu.vn>
2025-11-02 21:33:46 +0700

    fix: Thông báo trên header

--- app/components/notification/header-popup/content/NotificationContent.tsx
+++ app/components/notification/header-popup/content/NotificationContent.tsx
@@ -74,7 +74,7 @@ export function NotificationPopup() {
         <Button variant="ghost" size="icon" className="relative border border-gray-300">
           <Bell className="h-5 w-5" />
           {unreadCount > 0 && (
-            <span className="absolute top-[1px] right-[1px] flex h-3 w-3 items-center justify-center rounded-full bg-red-500 text-[10px] font-semibold text-white">
+            <span className="absolute -top-1.5 -right-1.5 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-[9px] font-semibold text-white border border-white">
               {unreadCount > 9 ? "9+" : unreadCount}
             </span>
           )}
